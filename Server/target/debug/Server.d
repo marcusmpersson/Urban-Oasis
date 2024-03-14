4a07b0f1a25fd81e42334de87f4a4dddf219d374
Marcus <thriftycryon@gmail.com>
2024-03-14 20:06:07 +0100

    Rust Server v0.1
    2024-03-14

---
+++ Server/target/debug/Server.d
@@ -0,0 +1,1 @@
+C:\Users\Marcus\Desktop\Urban-Oasis\Server\target\debug\Server.exe: C:\Users\Marcus\Desktop\Urban-Oasis\Server\src\main.rs
---
+++ Server/target/debug/deps/Server.d
@@ -0,0 +1,5 @@
+C:\Users\Marcus\Desktop\Urban-Oasis\Server\target\debug\deps\Server.exe: src\main.rs
+
+C:\Users\Marcus\Desktop\Urban-Oasis\Server\target\debug\deps\Server.d: src\main.rs
+
+src\main.rs:
