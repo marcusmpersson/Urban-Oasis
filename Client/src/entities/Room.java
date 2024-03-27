fdb2c6ff8edce4ce48d5a21485b3041568977460
ranaciita <rananoorzadeh@gmail.com>
2024-03-27 13:20:51 +0100

    entities: User, Inventory, Room, PlacementSlot

--- Client/src/entities/Room.java
+++ Client/src/entities/Room.java
@@ -1,4 +1,30 @@
 package entities;
 
+import java.util.ArrayList;
+
 public class Room {
+    private ArrayList<PlacementSlot> slots;
+
+
+    public Room(ArrayList<PlacementSlot> slots){
+        this.slots = slots;
+    }
+    public PlacementSlot getSlot(int index){
+        if (index < slots.size()) {
+            return slots.get(index);
+        }
+        return null;
+    }
+
+    /** places the given placeable item at given plcaementslot index, returns the item that was
+     * placed in the slot prior, returns null if slot was empty */
+    public Placeable placeItemIn(Placeable placeableItem, int index){
+        if (index < slots.size()) {
+            Placeable swappingItem = slots.get(index).getPlacedItem();
+            slots.get(index).setPlacedItem(placeableItem);
+            return swappingItem;
+        }
+        return null;
+    }
+
 }
