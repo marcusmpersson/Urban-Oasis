06ee7543b7f02dfbbdb94e03ab2307822ace99cd
Momme <mouhammedfakhro@outlook.com>
2024-03-30 13:20:40 +0100

    test

--- Client/src/entities/Pot.java
+++ Client/src/entities/Pot.java
@@ -3,6 +3,8 @@ import javafx.scene.image.Image;
 
 public class Pot extends Deco {
 
+
+    //test line
     public Pot(String name, Image image, int price){
         super.name = name ;
         super.price = price;
