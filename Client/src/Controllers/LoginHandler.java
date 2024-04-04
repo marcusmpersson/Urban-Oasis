1ddaf83d94b62c2c187461ffb2f07f6ee0d41080
StorckC <storck-en@hotmail.com>
2024-04-04 17:36:04 +0200

    LoginHandler can now handle logins. SystemController created with the new login function added.

--- Client/src/Controllers/LoginHandler.java
+++ Client/src/Controllers/LoginHandler.java
@@ -1,4 +1,37 @@
 package Controllers;
-
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import java.io.IOException;
 public class LoginHandler {
+    private CloseableHttpClient httpClient;
+    private HttpPost httpPost;
+
+    public LoginHandler(){
+        this.httpClient = HttpClients.createDefault();
+        this.httpPost = new HttpPost("/login");
+    }
+    public String login(String userName, String password){
+        try{
+            String requestBody = "{\"email\": \"" + userName + "\", \"password\": \"" + password + "\"}";
+            StringEntity entity = new StringEntity(requestBody);
+            httpPost.setEntity(entity);
+            try(CloseableHttpResponse response = httpClient.execute(httpPost)){
+                HttpEntity responseEntity = response.getEntity();
+                if(entity != null){
+                    return EntityUtils.toString(responseEntity);
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public void register(String email, String userName, String password){
+
+    }
 }
