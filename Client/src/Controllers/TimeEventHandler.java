820bac0b59bf941fc5d9cc229e43a02608f21bcb
ranaciita <rananoorzadeh@gmail.com>
2024-04-05 12:43:59 +0200

    TimeEventHandler

---
+++ Client/src/Controllers/TimeEventHandler.java
@@ -0,0 +1,15 @@
+package Controllers;
+
+public class TimeEventHandler extends Thread {
+
+    private GameHandler gameHandler;
+
+    public TimeEventHandler(GameHandler gameHandler){
+        this.gameHandler = gameHandler;
+    }
+
+    @Override
+    public void run(){
+
+    }
+}
