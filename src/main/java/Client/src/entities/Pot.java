e4e2ce62bf3e7b597939627f4fd018d5258b9fdb
ranaciita <rananoorzadeh@gmail.com>
2024-04-18 20:53:25 +0200

    itemBuilder class

--- src/main/java/Client/src/entities/Pot.java
+++ src/main/java/Client/src/entities/Pot.java
@@ -1,9 +1,18 @@
 package entities;
 
+import enums.PotType;
+
 public class Pot extends Deco {
 
-    public Pot(String name, String imageFilePath, int price){
+    private PotType type;
+
+    public Pot(String name, String imageFilePath, int price, PotType type){
         super(name, imageFilePath, price);
+        this.type = type;
+    }
+
+    public PotType getPotType(){
+        return type;
     }
 
 }
