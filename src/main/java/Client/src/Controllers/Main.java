5c57004261615f2ef0286db5d07faaca8159bbfa
StorckC <storck-en@hotmail.com>
2024-05-06 22:15:55 +0200

    Weather API is running. Added it to controller.

--- src/main/java/Client/src/Controllers/Main.java
+++ src/main/java/Client/src/Controllers/Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         Controller controller = Controller.getInstance();
-        WeatherUpdater weatherUpdater = new WeatherUpdater();
-        weatherUpdater.getCurrentWeather();
 
         // Glöm inte att stänga av clientConnections httpClient när man stänger av programmet.
 
