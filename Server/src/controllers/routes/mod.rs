6a2996d6776ab4271f8bfb96068076b9184d80e0
Marcus <thriftycryon@gmail.com>
2024-05-29 23:46:07 +0200

    last commit

--- Server/src/controllers/routes/mod.rs
+++ Server/src/controllers/routes/mod.rs
@@ -1,4 +1,3 @@
 pub(crate) mod plants;
 pub(crate) mod user;
 pub(crate) mod store;
-pub(crate) mod weather;
--- Server/src/private_cons.rs
+++ Server/src/private_cons.rs
@@ -1,3 +1,3 @@
-pub const JWT_SECRET: &'static str = "<secret>";
+pub const JWT_SECRET: &'static str = "your jwt secret";
 pub const REFRESH_SECRET: &'static str = "refresh_secret";
-pub(crate) const URL_DB: &str = "mongodb+srv://admin:igXWJEOYuEgN7zJj@urbanoasis.iga2ujs.mongodb.net/?retryWrites=true&w=majority&appName=UrbanOasis";
+pub(crate) const URL_DB: &str = "your-mongodb-link";
